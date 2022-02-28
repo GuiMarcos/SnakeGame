97982fa98036f86be504ace5abb424d403c57569
Guilherme Marcos <58338699+GuiMarcos@users.noreply.github.com>
2022-02-27 22:26:33 -0300

    Update main.js

--- Snake/assets/js/main.js
+++ Snake/assets/js/main.js
@@ -1,3 +1,4 @@
+function main(){
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const fps = 20;
@@ -166,3 +167,5 @@ window.addEventListener('keydown', (e) => {
         }
     }, 1);
 })
+}
+main();
